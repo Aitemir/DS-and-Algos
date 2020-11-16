875a0ae5217e37d41b8ac087051b93acaa0e4fa9
Aitemir Yeskenov <aitemir@aitemirs-mbp.home>
2020-11-15 19:11:58 -0500

    updated binary search

--- binarySearch.js
+++ binarySearch.js
@@ -2,18 +2,18 @@ let array = [3, 4, 6, 8, 9, 14, 15, 19, 21];
 
 function binarySearch(val, arr) {
 
-    let lower = 0;
-    let upper = arr.length - 1;
+    let left = 0;
+    let right = arr.length - 1;
     
-    while (lower <= upper) {
-        const middle = Math.floor((lower + upper) / 2)
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2)
         if (val === arr[middle]) {
             return middle;
         }
         if (val < arr[middle]) {
-            upper = middle - 1;
+            right = middle - 1;
         } else {
-            lower = middle + 1;
+            left = middle + 1;
         }
     }
     return - 1;
