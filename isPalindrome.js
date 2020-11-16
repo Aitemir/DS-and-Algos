4a1047a2a611b39232cf0dc18b9af76562449c14
Aitemir Yeskenov <aitemir@aitemirs-mbp.home>
2020-11-15 22:06:34 -0500

    added the most optimal  palindrom algorithm

---
+++ isPalindrome.js
@@ -0,0 +1,14 @@
+function isPalindrome(string) {
+  let leftIndex = 0;
+	let rightIndex = string.length - 1;
+	while (leftIndex < rightIndex) {
+		if (string[leftIndex] !== string[rightIndex]) {
+			return false; 
+		}
+		leftIndex++;
+		rightIndex--;
+	}
+	return true;
+}
+
+console.log(isPalindrome("abcdcba"));
