b509dae33905292816c7408753b4cbeb970634f3
Aitemir Yeskenov <aitemir@Aitemirs-MBP.localdomain>
2020-11-18 17:15:07 -0500

    updated isPalindrome.js file - fixed indentation inconsistency

--- isPalindrome.js
+++ isPalindrome.js
@@ -2,9 +2,10 @@
 // which guarantees O (n) time and O (1) space complexity
 
 function isPalindrome(string) {
-  let leftIndex = 0;
-	let rightIndex = string.length - 1;
-	while (leftIndex < rightIndex) {
+    let leftIndex = 0;
+    let rightIndex = string.length - 1;
+    
+    while (leftIndex < rightIndex) {
 		if (string[leftIndex] !== string[rightIndex]) {
 			return false; 
 		}
